3de2081d001eafc12a23cd7dd01390593b4e55a1
Malu <103540561+maluvsb@users.noreply.github.com>
2023-11-22 17:15:36 -0300

    Update rpg.h

--- rpg.h
+++ rpg.h
@@ -3,7 +3,6 @@
 #ifndef RPG_H
 #define RPG_H
 
-// Estrutura para representar um personagem
 typedef struct {
     char nome[50];
     int vida;
@@ -11,13 +10,10 @@ typedef struct {
     int defesa;
 } Personagem;
 
-// Função para inicializar um personagem
 Personagem* inicializaPersonagem(const char* nome, int vida, int ataque, int defesa);
 
-// Função para realizar um ataque entre dois personagens com um fator aleatório
 void atacar(Personagem* atacante, Personagem* defensor);
 
-// Função para realizar uma ação de defesa com um fator aleatório
 void defender(Personagem* defensor);
 
 #endif
