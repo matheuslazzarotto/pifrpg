7a2e6a78d965b400b392654ce7aee68f2f630284
matheus lazzarotto <99198452+matheuslazzarotto@users.noreply.github.com>
2023-11-22 19:01:32 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,34 @@
 #include <stdlib.h>
 #include <time.h>
 
+void printStartScreen() {
+    printf("*******************************************\n");
+    printf("*          BEM-VINDO AO RPGPIF            *\n");
+    printf("*******************************************\n");
+    printf("*                                         *\n");
+    printf("*  Você, o poderoso bárbaro, está diante  *\n");
+    printf("*  do Labirinto das Sombras. Sua aventura *\n");
+    printf("*  começa agora!                          *\n");
+    printf("*                                         *\n");
+    printf("*******************************************\n");
+
+void printBarbarianIntro() {
+    printf("\nCom seu machado em mãos e vestindo uma armadura de couro robusta,\n");
+    printf("você adentra o sombrio Labirinto das Sombras. A escuridão engole a luz de sua\n");
+    printf("tocha, deixando apenas o eco de seus passos reverberando pelas paredes de pedra.\n");
+    printf("Seu coração pulsa forte, ansioso pelo desconhecido que aguarda nas profundezas.\n");
+}
+
+void printEnemyEncounter() {
+    printf("\nSubitamente, olhos amarelos brilham na penumbra, seguidos do rosnado de uma fera.\n");
+    printf("Um goblin emerge das sombras, suas presas à mostra e garras prontas para o ataque.\n");
+    printf("Você se prepara para o combate, a adrenalina correndo por suas veias.\n");
+}
 int main() {
     srand(time(NULL));
 
+    printStartScreen();
+    
     Personagem* heroi = inicializaPersonagem("Herói", 100, 20, 10);
     Personagem* vilao = inicializaPersonagem("Vilão", 80, 15, 8);
 
